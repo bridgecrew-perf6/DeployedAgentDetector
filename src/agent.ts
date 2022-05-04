c9348da658a5e7e5127b943fc2d8c24125dea215
kushalpokharel <kusaalpokharel@gmail.com>
2022-05-04 15:17:06 +0545

    findings corrected

--- src/agent.ts
+++ src/agent.ts
@@ -9,7 +9,7 @@ import {
 } from "forta-agent";
 
 export const FUNCTION_NAME = "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
-export const DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
+export const DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8".toLowerCase();
 export const CONTRACT_ADDRESS = "0x61447385b019187daa48e91c55c02af1f1f3f863";
 let findingsCount = 0;
 
@@ -17,21 +17,20 @@ const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = [];
-
+  
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
-
+  if(txEvent.from.toLowerCase() !== DEPLOYER_ADDRESS)
+    return findings;
   // filter the transaction logs for CreateAgent function call
   const deployEvents = txEvent.filterFunction(
-    FUNCTION_NAME,
+    [FUNCTION_NAME],
     CONTRACT_ADDRESS
   );
   console.log(deployEvents);
   deployEvents.forEach((deployEvent) => {
     
-    const { to, from, value } = deployEvent.args;
     // console.log(deployEvent.args);
-    if(from===DEPLOYER_ADDRESS){
       findings.push(
         Finding.fromObject({
           name: "New Agent  Deployed",
@@ -40,13 +39,15 @@ const handleTransaction: HandleTransaction = async (
           severity: FindingSeverity.Low,
           type: FindingType.Info,
           metadata: {
-            to,
-            from,
+            "name":deployEvent.functionFragment.name,
+            "signature":deployEvent.signature,
+            "sigHash" : deployEvent.sighash,
+            
           },
         })
       );
       findingsCount++;
-    }
+    
   });
 
   return findings;
